8112b0658ac5a2e9899f95ddf1751f0c8760b7e6
AilsonETE <softpop.tecnologia@gmail.com>
2023-09-14 19:57:50 -0300

    add

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask import render_template
 import sqlite3
 
-
-
 #cria o objeto app
 app = Flask(__name__ , static_folder='static')
 
@@ -13,6 +11,33 @@ def get_db_connection():
     conn = sqlite3.connect('database\sqlite.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+# Rota para o painel de administração
+@app.route("/admin")
+def admin_panel():    
+    return render_template('admin/painel.html')
+
+# Rota para listar categorias
+@app.route("/admin/listar_categorias")
+def listar_categorias():
+    conn = get_db_connection()
+    categorias = conn.execute('SELECT * FROM categorias').fetchall()
+    conn.close()
+    return render_template('admin/listar_categorias.html', categorias=categorias)
+
+# Rota para cadastrar categoria
+@app.route("/admin/cadastrar_categoria")
+def cadastrar_categoria():
+    # Implemente o código para cadastrar categoria
+    return render_template('admin/cadastrar_categoria.html')
+
+# Rota para excluir categoria
+@app.route("/admin/excluir_categoria")
+def excluir_categoria():
+    # Implemente o código para excluir categoria
+    return render_template('admin/excluir_categoria.html')
+
+
 
 # cria a rota index
 @app.route("/")
